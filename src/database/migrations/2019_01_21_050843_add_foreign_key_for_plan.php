36eba47abc71ce498cc593b4482ce1ff471588a2
therealdb <hello@quantumapps.xyz>
2019-01-23 22:07:23 -0700

    Fix incorrect migration class name

--- src/database/migrations/2019_01_21_050843_add_foreign_key_for_plan.php
+++ src/database/migrations/2019_01_21_050843_add_foreign_key_for_plan.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateShopifyChargesForeignKeyTable extends Migration
+class AddForeignKeyForPlan extends Migration
 {
     /**
      * Run the migrations.
